eb86959c71a669a7ab01d25d1531a83fc0e346ae
Andrew Fahrenholtz <andrew.fahrenholtz+github@gmail.com>
2017-11-02 22:53:03 -0500

    grab the full path

--- deploy.ps1
+++ deploy.ps1
@@ -1,2 +1,2 @@
-$yamlFiles = Get-ChildItem -Filter *.yaml | Select-Object -exp FileName
+$yamlFiles = Get-ChildItem -Filter *.yaml | Select-Object -exp FullName
 $yamlFiles | ForEach-Object {Copy-Item $_ -Destination "\\hassio\config" -Force}
