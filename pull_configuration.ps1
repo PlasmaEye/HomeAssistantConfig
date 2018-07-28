e3336655f1caaa1cc5d3344163f42d4a0b8564ce
Andrew Fahrenholtz <andrew.fahrenholtz+github@gmail.com>
2018-07-28 08:56:19 -0500

    add garage, recorder, weather, and start lovelace build out

--- pull_configuration.ps1
+++ pull_configuration.ps1
@@ -1,1 +1,2 @@
-Copy-Item -Path "\\hassio\config\configuration.yaml" -Force
+Copy-Item -Path "\\192.168.1.4\config\configuration.yaml" -Force
+Copy-Item -Path "\\192.168.1.4\config\customize.yaml" -Force
