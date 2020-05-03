7b240ecd16638a5a600665b14091696f125a1802
Andrew Fahrenholtz <andrew.fahrenholtz+github@gmail.com>
2020-05-03 15:38:20 -0500

    pull scripts

--- pull_configuration.ps1
+++ pull_configuration.ps1
@@ -1,2 +1,3 @@
 Copy-Item -Path "\\hassio.local\config\configuration.yaml" -Force
 Copy-Item -Path "\\hassio.local\config\automations.yaml" -Force
+Copy-Item -Path "\\hassio.local\config\scripts.yaml" -Force
