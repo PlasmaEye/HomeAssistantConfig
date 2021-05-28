78cf0193ed4150e0b16d1c8ab2f4851e74a31461
Andrew Fahrenholtz <andrew.fahrenholtz+github@gmail.com>
2021-05-27 20:37:47 -0500

    update

--- pull_configuration.ps1
+++ pull_configuration.ps1
@@ -2,3 +2,9 @@ Copy-Item -Path "\\homeassistant.local\config\configuration.yaml" -Force
 Copy-Item -Path "\\homeassistant.local\config\automations.yaml" -Force
 Copy-Item -Path "\\homeassistant.local\config\scripts.yaml" -Force
 Copy-Item -Path "\\homeassistant.local\config\scenes.yaml" -Force
+Copy-Item -Path "\\homeassistant.local\config\sensors.yaml" -Force
+Copy-Item -Path "\\homeassistant.local\config\switches.yaml" -Force
+Copy-Item -Path "\\homeassistant.local\config\notifications.yaml" -Force
+Copy-Item -Path "\\homeassistant.local\config\customize.yaml" -Force
+Copy-Item -Path "\\homeassistant.local\config\cameras.yaml" -Force
+Copy-Item -Path "\\homeassistant.local\config\binary_sensors.yaml" -Force
